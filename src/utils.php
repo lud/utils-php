9eddd17fdad1f0e4048df6e6c7419a7539c8b5c3
niahoo <dev@ooha.in>
2014-12-11 19:16:51 +0100

    show the r() call line / file

--- src/utils.php
+++ src/utils.php
@@ -1,3 +1,10 @@
 <?php
 
-function r($x) { return dump_r($x); }
+function r($x) {
+	$trace = debug_backtrace(null,1);
+	$idx = strpos($trace[0]['file'], 'utils.php') ? 1 : 0;
+	$src = (object)$trace[$idx];
+	echo "<pre>call r() at {$src->file} line {$src->line}</pre>";
+	dump_r($x);
+	return $x;
+}
