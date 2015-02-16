371be177f467571dd17f1ef77be93ffba71e8737
niahoo <dev@ooha.in>
2015-02-16 10:51:27 +0100

    add renv() for dotenv require/get at once

--- src/utils.php
+++ src/utils.php
@@ -1,5 +1,20 @@
-<?php namespace Lud\Utils;
+<?php
 
-function toUTF8 ($str) {
-	return iconv(mb_detect_encoding($str, mb_detect_order(), true), "UTF-8", $str);
+namespace Lud\Utils {
+	function toUTF8 ($str) {
+		return iconv(mb_detect_encoding($str, mb_detect_order(), true), "UTF-8", $str);
+	}
+}
+
+namespace {
+	/**
+	 * Returns an environment variable, throws if the variable is not defined
+	 * @param  string $key           The environment key for the value
+	 * @param  array  $allowedValues An optional array of allowed values for the key
+	 * @return any                The environment value
+	 */
+	function renv($key,$allowedValues=array()) {
+		Dotenv::required($key,$allowedValues);
+		return (env($key));
+	}
 }
