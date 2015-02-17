64897ce37c77d9227de419f069835298d966fb73
niahoo <dev@ooha.in>
2015-02-17 12:10:42 +0100

    added mnemo function for mnemoization

--- src/utils.php
+++ src/utils.php
@@ -4,9 +4,23 @@ namespace Lud\Utils {
 	function toUTF8 ($str) {
 		return iconv(mb_detect_encoding($str, mb_detect_order(), true), "UTF-8", $str);
 	}
+
+
+
 }
 
 namespace {
+
+	/**
+	 * returns the value returned from the call of the passed function, except
+	 * if the passed variable is a simple value. in this case, just return it
+	 * @param  any $value a value
+	 * @return any
+	 */
+	function get_value($value) {
+		return is_callable($value) ? call_user_func($value) : $value;
+	}
+
 	/**
 	 * Returns an environment variable, throws if the variable is not defined
 	 * @param  string $key           The environment key for the value
@@ -16,5 +30,22 @@ namespace {
 	function renv($key,$allowedValues=array()) {
 		Dotenv::required($key,$allowedValues);
 		return (env($key));
+	}
+
+	/**
+	 * Takes a key and a value, returns true if the value is different from the
+	 * previous call with the same key
+	 * @param  string $key   The key for mnemoization
+	 * @param  any $callable Closure | function name | any value. The function to call for check the difference. Can also be a simple value
+	 * @return bool true if the values differs
+	 */
+	function mnemo($key,$callable) {
+		static $store;
+		$value = get_value($callable);
+		if (!isset($store[$key]) || $store[$key] !== $value) {
+			$store[$key] = $value;
+			return true; // the value differs since it was not evaled
+		}
+		return false;
 	}
 }
